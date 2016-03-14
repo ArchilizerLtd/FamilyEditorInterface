afc4d770e65f2290a6e48653239f1e6a04bcc974
2adicted <dido.nenov@gmail.com>
2016-03-14 23:42:08 +0000

    External Event Handler document changed added.

--- FamilyEditorInterface/Interface.Designer.cs
+++ FamilyEditorInterface/Interface.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace FamilyEditorInterface
+﻿using System.Windows.Input;
+namespace FamilyEditorInterface
 {
     partial class Interface
     {
@@ -29,12 +30,13 @@
         private void InitializeComponent()
         {
             this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnExit
             // 
             this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExit.Location = new System.Drawing.Point(331, 226);
+            this.btnExit.Location = new System.Drawing.Point(389, 226);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 0;
@@ -42,16 +44,32 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoScrollMinSize = new System.Drawing.Size(0, 300);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(480, 237);
+            this.panel1.TabIndex = 1;
+            // 
             // Interface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(418, 261);
+            this.ClientSize = new System.Drawing.Size(504, 261);
             this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(520, 120);
             this.Name = "Interface";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Family Parameters";
             this.Load += new System.EventHandler(this.Interface_Load);
+            this.GotFocus += new System.EventHandler(this.Control1_GotFocus);
             this.ResumeLayout(false);
 
         }
@@ -59,5 +77,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
     }
 }
